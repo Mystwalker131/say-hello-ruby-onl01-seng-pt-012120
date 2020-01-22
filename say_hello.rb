d2e1ade9ee836fce194a2c15e80e4aaf5a015928
Benito Galvez <mystwalker131@gmail.com>
2020-01-22 06:20:34 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,19 @@
-# Build your say_hello method here
+def say_hello(name = "Kent Beck")
+  puts "Hello, #{name}"
+end
+
+
+# name = ("Kent Beck")
+
+# def say_hello(name)
+# puts "Hello,#{name}"
+
+# end
+# say_hello("Kent Beck")
+
+# Kent_Black
+say_hello("Kent Beck!")
+ 
+# The method prints this text to the screen:
+#Hello Kent Beck!
+#Kent_Beck
