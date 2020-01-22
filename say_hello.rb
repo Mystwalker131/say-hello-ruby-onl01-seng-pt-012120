66a2900536a2bba52fb8a8a9532c09ef8c6fa477
Benito Galvez <mystwalker131@gmail.com>
2020-01-22 06:30:26 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -2,7 +2,9 @@
 #   puts "Hello, #{name}"
 # end
 def say_hello(name="Kent Beck", language="Ruby Programmer")
-  puts "Hello #{name}! We heard you are a great #{language} programmer."
+  # puts "Hello #{name}! def greeting(name, language="Ruby")
+  puts "Hello, #{name}. 
+end
 end
 
 # name = ("Kent Beck")
